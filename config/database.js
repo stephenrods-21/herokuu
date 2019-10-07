7a373f5b7c671b69a9fb718c103e6b5a5be9fe62
unknown <stephen.rods21@gmail.com>
2019-10-07 17:35:23 +0530

    linked DB New Connection8

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 module.exports = {
-    database: 'mongodb://srodrigues21:SR9767458029@mongodb-5402-0.cloudclusters.net:10017/meanapp?authSource=admin',
-   // database: 'mongodb://localhost:27017/meanauth',
+    //database: 'mongodb://srodrigues21:SR9767458029@mongodb-5402-0.cloudclusters.net:10017/meanapp?authSource=admin',
+    database: 'mongodb://localhost:27017/meanauth',
     secret: 'yoursecret'
 }
 
