596d3be0634d25266b320b4ddf5ccd5cd57ecffc
unknown <stephen.rods21@gmail.com>
2019-10-07 15:43:17 +0530

    linked DB New Connection

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 module.exports = {
-    database: 'mongodb+srv://stephenrods21:Sr9767458029@cluster0-ph5vt.mongodb.net/admin?retryWrites=true&w=majority',
+    database: 'mongodb+srv://stephenrods21:SR9767458029@cluster0-ph5vt.mongodb.net/admin?retryWrites=true&w=majority',
    // database: 'mongodb://localhost:27017/meanauth',
     secret: 'yoursecret'
 }
