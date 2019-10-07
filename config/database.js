a744acc201cd0377df5761e73de4212226fd7888
unknown <stephen.rods21@gmail.com>
2019-10-07 15:20:03 +0530

    linked DB

--- config/database.js
+++ config/database.js
@@ -1,4 +1,5 @@
 module.exports = {
-    database: 'mongodb://localhost:27017/meanauth',
+    database: 'mongodb+srv://stephenrods21:Sr9767458029@cluster0-ph5vt.mongodb.net/admin?retryWrites=true&w=majority',
+   // database: 'mongodb://localhost:27017/meanauth',
     secret: 'yoursecret'
 }
