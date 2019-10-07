33204c2580967fe0a2053e78cba9ae6367535009
unknown <stephen.rods21@gmail.com>
2019-10-07 16:10:08 +0530

    linked DB New Connection4

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 module.exports = {
-    database: 'mongodb+srv://stephenrods21:SR9767458029@cluster0-ph5vt.mongodb.net/admin?retryWrites=true&w=majority',
+    database: 'mongodb://srodrigues21:SR9767458029@mongodb-5402-0.cloudclusters.net:10017/meanapp?authSource=admin',
    // database: 'mongodb://localhost:27017/meanauth',
     secret: 'yoursecret'
 }
