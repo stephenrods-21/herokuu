4c38bf4181ba38bcead3c31211dff382eff01de3
unknown <stephen.rods21@gmail.com>
2019-10-09 17:31:19 +0530

    linked DB New Connection19

--- app.js
+++ app.js
@@ -17,8 +17,8 @@ mongoose.connection.on('error',(err)=>{
 });
 
 const app = express();
-const port = 3000;
-//const port = process.env.PORT || 8080;
+//const port = 3000;
+const port = process.env.PORT || 8080;
 
 //CORS Middleware
 app.use(cors());
